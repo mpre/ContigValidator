dadee13ce8327fd000d7fd65dfedff94a4749869
prj_errorcorr <prj_errorcorr@localhost>
2019-05-05 16:04:50 +0200

    Rename exectuables and add threads param to cv

--- ContigValidator.sh
+++ ContigValidator.sh
@@ -37,6 +37,7 @@ NC='\033[0m' #No Color
 kmersize=30
 abundancemin=1
 referenceAbundanceMin=1
+threads=16
 
 optionInfo="-r -s -i|-f are compulsory options. -i can be multiple \n\
 -r | --reference filename -> for passing the reference file\n\
@@ -44,6 +45,7 @@ optionInfo="-r -s -i|-f are compulsory options. -i can be multiple \n\
 -i | --input filename -> filename contains the input files to be checked\n\
 -a | --alignment filename -> filename is the file in which alignment results will be shown. Default Value is alignmentResults.txt\n\
 -f | --file filename -> filename is a file which contains the input filenames\n\
+-t | --threads int -> number of threads used\n\
 -suffixskip 1 -> to skip the Suffix Tree Creation. The default value is 0 and will create the Suffix Tree\n\
 -suffixsave 0 -> to not save the Suffix Tree. The default value is 1 and will save the Suffix Tree\n\
 -bwaskip 1 -> to skip the bwa step. The default value is 0 and it will do the bwa step\n\
@@ -57,6 +59,7 @@ optionInfo="-r -s -i|-f are compulsory options. -i can be multiple \n\
 while [ "$1" != "" ]; do
     case $1 in
         -r | --reference )           shift
+	    echo $1
             referenceGenome=$1
             referenceGenomeArray+=($1)
             flagReference=1
@@ -77,6 +80,9 @@ while [ "$1" != "" ]; do
         -f | --file )			shift
 	    files=$1
 	    flagInput=1
+	    ;;
+	-t | --threads )                shift
+	    threads=$1
 	    ;;
         -suffixskip )			shift
 	    suffixskip=$1
@@ -163,21 +169,21 @@ fi
 > $multipleGenomeFile
 
 ##concatenate every file to multipleGenomeFile
-cat ${referenceGenomeArray} > $multipleGenomeFile
+cat ${referenceGenomeArray[@]} > $multipleGenomeFile
 
 ##the first reference file given is assumed as the main reference file without any mutations or substitutions
 referenceGenome=${referenceGenomeArray[0]}
 
 ## Suffix Tree for simple exact matching
-echo "Running C++ Suffix Tree"
-cd src
-make 
-if [ $? -ne 0 ]; then
-    echo "Compilation failed.  Please check that sdsl is in your include path and in your library path."
-    cd ..
-    sane_quit
-fi
-cd ..
+# echo "Running C++ Suffix Tree"
+# cd src
+# make 
+# if [ $? -ne 0 ]; then
+#     echo "Compilation failed.  Please check that sdsl is in your include path and in your library path."
+#     cd ..
+#     sane_quit
+# fi
+# cd ..
 
 ## Adding filenames to the $alignment output file
 echo "Filename" > $alignment
@@ -191,7 +197,7 @@ done
 echo "Checking the input files against the tree"
 
 ##Performing it with multipleGenomeFile
-src/program $multipleGenomeFile $tmpsuffixfile $inputFiles > $tmpoutfile 2> $tmperrorfile
+cv_st_based $multipleGenomeFile $tmpsuffixfile $inputFiles > $tmpoutfile 2> $tmperrorfile
 
 if [ $? = 0 ]; then
     cat $tmpoutfile
@@ -229,7 +235,7 @@ if [ "$bwaskip" = 0 ]; then
     for i in "${inputFileArray[@]}"
     do
 	output="$i.bwa.bam"
-	bwa mem -t 16 $referenceGenome $i | samtools sort > $output 2> $tmperrorfile
+	bwa mem -t $threads $referenceGenome $i | samtools sort > $output 2> $tmperrorfile
 	if [ "$?" -gt 0 ]; then
 	    printf "${RED}ERROR - "
 	    cat $tmperrorfile
@@ -262,27 +268,24 @@ fi
 # USE KMC FOR BETTER PERFOMANCE
 cp $alignment old-alignment.txt
 
-cd ${SCRIPT_PATH}/src
-make
-cd ${SCRIPT_PATH}
 
-tmpKmerOut="/tmp/tmpkmerout.txt"
+tmpKmerOut="tmpkmerout.txt"
 if [ "$kmerskip" = 0 ]; then
     echo "Kmer size = $kmersize"
     echo "Abundance Min = $abundancemin"
 
-    ${SCRIPT_PATH}/src/KMC/bin/kmc -t4 -ci1 -k$kmersize -fm $multipleGenomeFile mg /tmp
+    kmc -t4 -ci1 -k$kmersize -fm $multipleGenomeFile mg .
 
     echo -e "recall\tprecision" > $tmpKmerOut
     for i in "${inputFileArray[@]}"
     do
-        ${SCRIPT_PATH}/src/KMC/bin/kmc -t4 -ci$abundancemin -k$kmersize -fm $i $i.kmc /tmp
-        ${SCRIPT_PATH}/src/KMC/bin/kmc_tools simple mg -ci1 $i.kmc -ci1 intersect $i.tp -ci1
-        ${SCRIPT_PATH}/src/KMC/bin/kmc_tools simple mg -ci1 $i.kmc -ci1 kmers_subtract $i.fn -ci1
-        ${SCRIPT_PATH}/src/KMC/bin/kmc_tools simple mg -ci1 $i.kmc -ci1 reverse_kmers_subtract $i.fp -ci1
-        tp=$(./src/count_kmers_kmc $i.tp)
-        fn=$(./src/count_kmers_kmc $i.fn)
-        fp=$(./src/count_kmers_kmc $i.fp)
+        kmc -t4 -ci$abundancemin -k$kmersize -fm $i $i.kmc .
+        kmc_tools simple mg -ci1 $i.kmc -ci1 intersect $i.tp -ci1
+        kmc_tools simple mg -ci1 $i.kmc -ci1 kmers_subtract $i.fn -ci1
+        kmc_tools simple mg -ci1 $i.kmc -ci1 reverse_kmers_subtract $i.fp -ci1
+        tp=$(count_kmers_kmc $i.tp)
+        fn=$(count_kmers_kmc $i.fn)
+        fp=$(count_kmers_kmc $i.fp)
         precision=$(bc -l <<< "scale=4; $tp * 100 / ($tp + $fp)")
         recall=$(bc -l <<< "scale=4; $tp * 100 / ($tp + $fn)")
         echo -e "$recall%\t$precision%" >> $tmpKmerOut
@@ -293,8 +296,8 @@ fi
 
 if [ "$clean" = 0 ]; then
     echo "In clean"
-    rm -f $referenceGenome.*
-    rm -f *.kmc_pre *.kmc_suf
+    # rm -f $referenceGenome.*
+    # rm -f *.kmc_pre *.kmc_suf
 fi
 
 sane_quit
